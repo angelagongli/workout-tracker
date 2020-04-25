0136fa3512410010a8bac9edc7175c5c3484e6f7
angela <angelagongli@gmail.com>
2020-04-24 21:55:00 -0500

    add mongoose model files Workout.js and index.js

---
+++ models/Workout.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+
+  exercises: Array,
+
+  totalDuration: {
+    type: Number,
+    default: function() {
+      let total = 0;
+      if (this.exercises) {
+        this.exercises.forEach(exercise => {
+          total += exercise.duration;
+        });
+      }
+      return total;
+    }
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
